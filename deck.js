b911268caade714dd2e34162a14313966c77b9c1
Brandon Lucas <thebrandonlucas@gmail.com>
2022-06-02 16:39:13 -0400

    feat: random shuffle function

--- deck.js
+++ deck.js
@@ -1,7 +1,6 @@
 function makeDeck() {
     var ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];
     var suits = ['♦', '♣', '♥', '♠'];
-    console.log(suits);
     var deck = [];
     // Generate deck
     for (var _i = 0, suits_1 = suits; _i < suits_1.length; _i++) {
@@ -13,6 +12,23 @@ function makeDeck() {
     }
     return deck;
 }
+function randomIntInRange(min, max) {
+    // if min = 0 and max = 51,
+    // (max - min + 1) + min 
+    // = (51 - 0 + 1) + 0 
+    // = 52
+    // Math.random() is exclusive on the upper bound, will never return 52
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+// Fisher-Yates Shuffle: https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+function shuffle(deck) {
+    for (var i = deck.length - 1; i > 0; i--) {
+        var randomIndex = randomIntInRange(0, i);
+        var temp = deck[i];
+        deck[i] = deck[randomIndex];
+        deck[randomIndex] = temp;
+    }
+}
 var deck = makeDeck();
+shuffle(deck);
 console.log(deck);
-console.log(deck.length);
--- deck.ts
+++ deck.ts
@@ -6,14 +6,11 @@ type Card = {
   suit: Suit;
 }
 
+// TODO: JSDoc for functions
 
 function makeDeck(): Card[] {
-
   const ranks: Rank[] = [2,3,4,5,6,7,8,9,10,'J','Q','K','A'];
   const suits: Suit[] = ['♦', '♣', '♥', '♠'];
-
-
-  console.log(suits)
   let deck: Card[] = [];
   // Generate deck
   for (const suit of suits) {
@@ -21,8 +18,34 @@ function makeDeck(): Card[] {
       deck.push({rank,suit})
     }
   }
-  
   return deck
 }
 
+function randomIntInRange(min: number, max: number) {
+  // if min = 0 and max = 51,
+  // (max - min + 1) + min 
+  // = (51 - 0 + 1) + 0 
+  // = 52
+  // Math.random() is exclusive on the upper bound, will never return 52
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+// Fisher-Yates Shuffle: https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+function shuffle(deck: Card[]) {
+  for (let i = deck.length - 1; i > 0; i--) {
+    const randomIndex = randomIntInRange(0, i);
+    const temp = deck[i];
+    deck[i] = deck[randomIndex];
+    deck[randomIndex] = temp;
+  }
+}
+
+function top5() {
+
+}
+
 const deck: Card[] = makeDeck()
+
+shuffle(deck)
+
+console.log(deck);
